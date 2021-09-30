21568960180ba9eb3d8c3f5754580fd208001d51
Panagiotis Skarlas <pskarlas@users.noreply.github.com>
2021-09-30 13:32:44 +0300

    - fixed typo in reflections.rb - setup of controller/routing

---
+++ app/controllers/v1/countries_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# app/controllers/V1/countries_controller.rb
+class V1::CountriesController < ApplicationController
+  def guess; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@
 
 # app/config/routes.rb
 Rails.application.routes.draw do
+  concern :base_api do
+    get '/country_guess', to: 'countries#guess'
+  end
 
+  namespace :v1, defaults: {format: :json} do
+    concerns :base_api
+  end
 end
