ac476db83be3be591493ce4c9f16c8a2b3b22cc1
Panagiotis Skarlas <pskarlas@users.noreply.github.com>
2021-10-05 14:53:39 +0300

    - cache responses for 24 hrs

--- app/controllers/v1/countries_controller.rb
+++ app/controllers/v1/countries_controller.rb
@@ -3,19 +3,21 @@
 # app/controllers/V1/countries_controller.rb
 class V1::CountriesController < ApplicationController
 
+  before_action :set_name, :set_cache_key
+
   def guess
     start = starting_execution
-    person_name = params[:name]
-
-    # Return early and give feedback for clients bad request
-    return bad_request_for(person_name) unless valid_name?(person_name)
-
-    guessed_country = NamsorServices::DetermineCountryFromSurname.new(person_name).call
+    # Give feedback on bad input early
+    return bad_request_for(@person_name) unless valid_name?(@person_name)
+    # Check cache
+    guessed_country = Rails.cache.fetch(@cache_key, expires_in: 24.hours) do
+      NamsorServices::DetermineCountryFromSurname.new(@person_name).call
+    end
 
     if guessed_country.success?
       render json: {
         guessed_country: guessed_country.payload,
-        requested_name: person_name,
+        requested_name: @person_name,
         time_processed: elapsed_from(start)
       }, status: :ok
     else
@@ -25,6 +27,14 @@ class V1::CountriesController < ApplicationController
   end
 
   private
+
+  def set_name
+    @person_name = params[:name]
+  end
+
+  def set_cache_key
+    @cache_key = ["country_guess", @person_name]
+  end
 
   def bad_request_for(name)
     render json: { error: 'A valid name parameter is required.
